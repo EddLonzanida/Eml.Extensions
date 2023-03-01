ac1ba29e3f3a074ac2f45f79f162bedfaa5c94f5
Eddie Lonzanida <EddieLonzanida@hotmail.com>
2023-03-01 14:41:57 +0800

    Rename HasChanges parameteres to existingItem and incomingItem

--- Eml.Extensions/HasChangesDto.cs
+++ Eml.Extensions/HasChangesDto.cs
@@ -7,7 +7,7 @@ public class HasChangesDto
 {
     public string PropertyName { get; set; } = string.Empty;
 
-    public object? Value1 { get; set; }
+    public object? ExistingValue { get; set; }
 
-    public object? Value2 { get; set; }
+    public object? IncomingValue { get; set; }
 }
