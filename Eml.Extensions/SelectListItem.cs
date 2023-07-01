ffa16fcdac947b0ffb0629f2c44e6bc427727305
Eddie Lonzanida <EddieLonzanida@hotmail.com>
2023-07-01 19:34:39 +0800

    Target net6 and 7

--- Eml.Extensions/SelectListItem.cs
+++ Eml.Extensions/SelectListItem.cs
@@ -4,10 +4,16 @@ public class SelectListItem
 {
     public bool? Selected { get; set; }
 
+#if NET6
+    public string Text { get; set; } = string.Empty;
+
+    public string Value { get; set; } = string.Empty;
+#endif
+#if NET7
     public required string Text { get; set; }
 
     public required string Value { get; set; }
-
+#endif
     public string? SubGroup { get; set; }
 
     public string? SubGroup2 { get; set; }
